5cbecdc4c287f4fe42e4e8f0a3eb13f4fd1c5f24
Alexey Smirnov <Sibusky@yandex.ru>
2022-06-03 01:39:18 +0300

    feat: add Auth.js

---
+++ src/utils/Auth.js
@@ -0,0 +1,34 @@
+export const BASE_URL = 'https://auth.nomoreparties.co';
+
+// Проверяю ответ сервера
+function checkResponse(res) {
+    return res.ok
+        ? res.json()
+        : Promise.reject(`Ошибка...: ${res.status}`);
+};
+
+// Функция регистрации пользователя
+export function register(email, password) {
+    return fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then(checkResponse)
+};
+
+// Функция авторизации пользователя
+export function authorize(email, password) {
+    return fetch(`${BASE_URL}/signin`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then(checkResponse)
+};
