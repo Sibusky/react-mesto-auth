c4012f7f592100a66a3c9f3b0758aa1ef56dbc32
Alexey Smirnov <Sibusky@yandex.ru>
2022-06-03 01:28:33 +0300

    feat: add routes and links to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom';
 import logo from '../logo.svg';
 
-export default function Header() {
+export default function Header({ userData, handleLogOut }) {
     return (
-        <header className="header">
-            <img className="header__logo" src={logo} alt="Логотип" />
+        <header className='header'>
+            <Route>
+                <Link className='header__img' to='/' target='_self'>
+                    <img alt='Логотип' className='header__logo' src={logo} />
+                </Link>
+            </Route>
+
+            <Route path='/signup'>
+                <Link className='header__link link' to='/signin'>Войти</Link>
+            </Route>
+
+            <Route path='/signin'>
+                <Link className='header__link link' to='/signup'>Регистрация</Link>
+            </Route>
+
+            <Route exact path='/'>
+                <p className='header__email'>{userData.email}</p>
+                <Link className='header__link link' to='/signin' onClick={handleLogOut} >Выйти</Link>
+            </Route>
+
         </header>
-    )
+    );
 }
