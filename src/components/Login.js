8f6f9f3794f2b56fcbc103442af0bb323c09d7d9
Alexey Smirnov <Sibusky@yandex.ru>
2022-06-02 10:14:24 +0300

    feat: add Login component

---
+++ src/components/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Login({ handleLogin }) {
+
+    // Объявляю переменные состояния через хук useState
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+    });
+
+    // Универсальный обработчик полей
+    function handleChange(e) {
+        const { name, value } = e.target
+        setValues((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    // Обработчик формы
+    function handleSubmit(e) {
+        e.preventDefault();
+        const { email, password } = values;
+        handleLogin({ email, password });
+    }
+
+    return (
+        <div className='login'>
+            <h3 className='login__title'>Вход</h3>
+            <form className='login__form' onSubmit={handleSubmit}>
+                <label className='login__form-field'>
+                    <input className='login__input login__input_email'
+                        type='email'
+                        placeholder='Email'
+                        name='email'
+                        onChange={handleChange}
+                        value={values.email}
+                        required />
+                </label>
+                <span
+                    id='error-login-email'
+                    className='login__error'>
+                </span>
+                <label className='login__form-field'>
+                    <input className='login__input login__input_password'
+                        type='password'
+                        placeholder='Пароль'
+                        name='password'
+                        onChange={handleChange}
+                        value={values.password}
+                        required />
+                </label>
+                <span
+                    id='error-login-email'
+                    className='login__error'>
+                </span>
+                <button className='login__submit' type='submit'>Войти</button>
+            </form>
+            <p className='register__signin'>Еще нет личного аккаунта? <Link to='/signup' className='register__signin_link'>Зарегистрироваться</Link></p>
+        </div>
+    );
+}
